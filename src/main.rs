5659159f1b070d09ca51318a6fff662436725726
Friedchicken-42 <paololego20+github@gmail.com>
2024-02-15 16:36:12 +0100

    General update + cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{bail, Result};
 
-fn read_varint(data: &[u8]) -> (u64, &[u8], usize) {
+fn read_varint(data: &[u8]) -> (u64, usize) {
     let mut i = 0;
     let mut number = 0u64;
     loop {
@@ -19,22 +19,23 @@ fn read_varint(data: &[u8]) -> (u64, &[u8], usize) {
         number = number << 7 | data[i] as u64;
     }
 
-    return (number, &data[i + 1..], i + 1);
+    (number, i + 1)
 }
 
-fn parse_bytearray(data: &[u8]) -> (u64, Vec<u64>) {
-    let (header_bytes, data, size) = read_varint(data);
-
-    let mut record_sizes = vec![];
+fn parse_bytearray(data: &[u8]) -> (Vec<usize>, usize) {
+    let (header_bytes, size) = read_varint(data);
 
     if header_bytes == 0 {
-        return (0, vec![]);
+        return (vec![], 0);
     }
-    let mut data = data;
+
+    let mut offset = size;
+    let mut record_sizes = vec![];
+
     let mut length = header_bytes - size as u64;
     while length > 0 {
-        let (value, d, size) = read_varint(data);
-        data = d;
+        let (value, size) = read_varint(&data[offset..]);
+        offset += size;
         length -= size as u64;
         let colunm_size = match value {
             0 | 8 | 9 | 12 | 13 => 0,
@@ -48,69 +49,107 @@ fn parse_bytearray(data: &[u8]) -> (u64, Vec<u64>) {
             n if n >= 13 && n % 2 == 1 => (n - 13) / 2,
             _ => unreachable!(),
         };
-        record_sizes.push(colunm_size);
+        record_sizes.push(colunm_size as usize);
     }
 
-    (header_bytes, record_sizes)
-}
-
-fn read_btree_page(data: &[u8]) -> (BTreePage, &[u8]) {
-    let (payload_bytes, data, _) = read_varint(&data);
-    let (_rowid, data, _) = read_varint(&data);
-    let bytearray = &data[..payload_bytes as usize];
-
-    let (offset, sizes) = parse_bytearray(bytearray);
-    let columns = &bytearray[offset as usize..];
-    let name = &columns[sizes[0] as usize..][..sizes[1] as usize];
-    let name = std::str::from_utf8(name).unwrap();
-
-    let offset_root: u64 = sizes[0..3].iter().sum();
-    let rootpage = columns[offset_root as usize];
-
-    let page = BTreePage { name, rootpage };
-
-    (page, &data[payload_bytes as usize..])
-}
-
-struct Header {
-    page_size: u16,
-}
-
-impl Header {
-    fn new(data: &[u8]) -> Self {
-        Self {
-            page_size: u16::from_be_bytes([data[16], data[17]]),
-        }
-    }
+    (record_sizes, header_bytes as usize)
 }
 
 #[derive(Debug)]
 struct BTreeHeader {
-    page_type: u8,
-    freeblock: u16,
-    cells: u16,
-    offset: u16,
-    frag: u8,
+    page_type: usize,
+    freeblock: usize,
+    cells: usize,
+    offset: usize,
+    frag: usize,
 }
 
 impl BTreeHeader {
-    fn new(data: &[u8]) -> Self {
+    fn read(data: &[u8]) -> Self {
         Self {
-            page_type: data[0],
-            freeblock: u16::from_be_bytes([data[1], data[2]]),
-            cells: u16::from_be_bytes([data[3], data[4]]),
-            offset: u16::from_be_bytes([data[5], data[6]]),
-            frag: data[7],
+            page_type: data[0] as usize,
+            freeblock: u16::from_be_bytes([data[1], data[2]]) as usize,
+            cells: u16::from_be_bytes([data[3], data[4]]) as usize,
+            offset: u16::from_be_bytes([data[5], data[6]]) as usize,
+            frag: data[7] as usize,
         }
     }
 }
 
 #[derive(Debug)]
-struct BTreePage<'a> {
-    name: &'a str,
-    rootpage: u8,
+struct Cell {
+    name: String,
+    rootpage: usize,
 }
 
+#[derive(Debug)]
+struct BTreePage {
+    header: BTreeHeader,
+    cells: Vec<Cell>,
+}
+
+impl BTreePage {
+    fn read(data: &[u8], offset: usize) -> Self {
+        let header_offset = if offset == 0 { 100 } else { offset };
+        let header = BTreeHeader::read(&data[header_offset..]);
+
+        let mut offset = header.offset + offset;
+        let mut cells = Vec::with_capacity(header.cells);
+
+        for _ in 0..header.cells {
+            let (payload_bytes, size) = read_varint(&data[offset..]);
+            offset += size;
+
+            let (_rowid, size) = read_varint(&data[offset..]);
+            offset += size;
+
+            let (sizes, size) = parse_bytearray(&data[offset..]);
+
+            let columns = offset + size;
+
+            let name = &data[columns + sizes[0]..][..sizes[1]];
+            // TODO: use slice
+            let name = String::from_utf8(name.to_vec()).unwrap();
+
+            let offset_rootpage: usize = sizes[0..3].iter().sum();
+            let rootpage = data[columns + offset_rootpage] as usize;
+
+            offset += payload_bytes as usize;
+
+            cells.push(Cell { name, rootpage });
+        }
+        Self { header, cells }
+    }
+}
+
+struct Header {
+    page_size: usize,
+}
+
+impl Header {
+    fn read(data: &[u8]) -> Self {
+        let page_size = u16::from_be_bytes([data[16], data[17]]) as usize;
+        Self { page_size }
+    }
+}
+
+struct Sqlite {
+    data: Vec<u8>,
+    header: Header,
+}
+
+impl Sqlite {
+    fn read(file: &str) -> Result<Self> {
+        let data = std::fs::read(file)?;
+        let header = Header::read(&data);
+        Ok(Self { data, header })
+    }
+
+    fn btreepage(&self, index: usize) -> BTreePage {
+        let offset = (index - 1) * self.header.page_size;
+        BTreePage::read(&self.data, offset)
+    }
+}
 fn main() -> Result<()> {
     // Parse arguments
     let args = std::env::args().collect::<Vec<_>>();
@@ -120,46 +159,58 @@ fn main() -> Result<()> {
         _ => {}
     }
 
-    let data = std::fs::read(&args[1])?;
+    let db = Sqlite::read(&args[1])?;
 
     // Parse command and act accordingly
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let header = Header::new(&data);
+            println!("database page size: {}", db.header.page_size);
 
-            println!("database page size: {}", header.page_size);
-
-            let schema_header = BTreeHeader::new(&data[100..]);
-            println!("number of tables: {}", schema_header.cells);
+            let page = db.btreepage(1);
+            println!("number of tables: {}", page.header.cells);
         }
         ".tables" => {
-            let schema_header = BTreeHeader::new(&data[100..]);
+            let page = db.btreepage(1);
 
-            let mut cell = &data[schema_header.offset as usize..];
-            for _ in 0..schema_header.cells {
-                let (page, c) = read_btree_page(cell);
-                cell = c;
-                if page.name != "sqlite_sequence" {}
+            let tables = page.cells.iter().filter(|p| p.name != "sqlite_sequence");
+
+            for table in tables {
+                print!("{} ", table.name);
             }
-            println!();
         }
         command => {
-            let table = command.split(" ").last().unwrap();
+            let commands: Vec<_> = command.split(" ").collect();
+            let table = commands[3];
 
-            let header = Header::new(&data);
-            let schema_header = BTreeHeader::new(&data[100..]);
+            let page = db.btreepage(1);
+            let Some(cell) = page.cells.iter().find(|p| p.name == table) else {
+                panic!("no table named \"{table}\"");
+            };
 
-            let mut cell = &data[schema_header.offset as usize..];
-            for _ in 0..schema_header.cells {
-                let (page, c) = read_btree_page(cell);
-                cell = c;
-                if page.name == table {
-                    let index = (page.rootpage - 1) as usize * header.page_size as usize;
-                    let page_header = BTreeHeader::new(&data[index..]);
-                    println!("{}", page_header.cells);
-                }
-            }
+            let page = db.btreepage(cell.rootpage);
+            println!("{}", page.header.cells);
+
+            // let name = commands[1];
+            // let table = commands[3];
+
+            // let header = Header::new(&data);
+            // let schema_header = BTreeHeader::new(&data[100..]);
+
+            // let mut cell = &data[schema_header.offset as usize..];
+            // for i in 0..schema_header.cells {
+            //     let (page, c) = read_btree_page(cell);
+            //     cell = c;
+            //     if page.name == table {
+            //         println!("{table} {i} {page:?}");
+            //         let index = (page.rootpage - 1) as usize * header.page_size as usize;
+            //         let page_header = BTreeHeader::new(&data[index..]);
+            //         println!("{page_header:?} {:#x}", page_header.offset as usize + index);
+
+            //         let mut cell = &data[page_header.offset as usize + index..];
+            //         let (page, c) = read_btree_page(cell);
+            //     }
+            // }
         }
     }
 
